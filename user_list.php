dd1154cfaf6a72f41eecaaeaf9a4f241e4fbf193
Jello1887 <89318864+Jello1887@users.noreply.github.com>
2023-12-02 15:18:38 -0800

    Update user_list.php
    Updated User cards to display new profile picture and to only be accessible to logged in user with admin role.

--- user_list.php
+++ user_list.php
@@ -1,6 +1,6 @@
 <?php
 $pageTitle = 'User List';
-include 'includes/header.php';
+include 'includes/header_session.php';
 require 'connect_db.php';
 ?>
 <body>
@@ -24,14 +24,23 @@ require 'connect_db.php';
             echo "<div class='oj-edit-bar {$statusClass}'>";
             echo "<span class='oj-user-id'>User ID: " . str_pad(htmlspecialchars($row['user_id']), 3, '0', STR_PAD_LEFT) . "</span>";
             echo "<div class='oj-icon-wrap'>";
-            echo "<a href='#' title='edit user'><i class='fa-solid fa-pen-to-square'></i></a>";
+            echo "<a href='edit_user.php?id=" . $row['user_id'] . "' title='edit user'><i class='fa-solid fa-pen-to-square'></i></a>";
             echo "<a href='{$deleteUrl}' title='{$deleteTitle}'><i class='fa-solid {$deleteIcon}'></i></a>";
             echo "</div>";
             echo "</div>";
+            echo "<div class='oj-user-info'>";
+            if (!empty($row['profile_picture']) && file_exists($row['profile_picture'])) {
+                echo "<div class='oj-profile-img-wrap'>";
+                echo "<img src='" . htmlspecialchars($row['profile_picture']) . "' alt='Profile Picture'>";
+                echo "</div>";
+            }
+            echo "<div class='oj-user-content'>";
             echo "<h2>" . htmlspecialchars($row['first_name']) . " " . htmlspecialchars($row['last_name']) . "</h2>";
             echo "<p><b>Email:</b> " . htmlspecialchars($row['email_address']) . "</p>";
             echo "<p><b>Location:</b> " . htmlspecialchars($row['city']) . ", " . htmlspecialchars($row['state']) . "</p>";
             echo "<p><b>Status:</b> {$statusText}</p>";
+            echo "</div>";
+            echo "</div>";
             echo "</div>";
         }
     } else {
@@ -42,4 +51,6 @@ require 'connect_db.php';
     </section>
 </body>
 </html>
+
+
 
